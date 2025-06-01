bd2eab8d0e024bc4c387475c73cf60aad38c2850
Abhisekh Padhy <abhisekhpadhy2005@gmail.com>
2025-06-01 11:53:46 +0530

    Update app.py to use dynamic port for Render

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,11 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
 import pandas as pd
+import os
+
+
+
+# Initialize Flask app
 
 app = Flask(__name__)
 CORS(app)
@@ -16,6 +21,7 @@ try:
 except Exception as e:
     print(f"Error loading models: {str(e)}")
     raise
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -63,4 +69,8 @@ def predict():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
+
+

8b0828dece9754b37760d56e39d0409f9ff14ce1
Tadvab Pradhan <tadvabpradhan2005@gmail.com>
2025-06-05 10:15:52 +0530

    changed the localhost

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -89,7 +89,7 @@ export default function App() {
     setPrediction("");
 
     try {
-      const response = await fetch('http://localhost:5000/predict', {
+      const response = await fetch('http://localhost:10000/predict', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,21 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()]
+  
+  
+}
+
   // tailwindcss()],
-})
+ )
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/predict': 'http://localhost:10000',
+//     }
+//   }
+// });
